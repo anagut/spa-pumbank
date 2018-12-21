83f5f3a5a73437a15965d6a7bfacf85bf56c1eb8
anagut <anagutierrezlazaro@gmail.com>
2018-12-21 09:01:06 +0100

    hola

--- Pumbank/src/main/java/com/pumbank/services/AnaService.java
+++ Pumbank/src/main/java/com/pumbank/services/AnaService.java
@@ -2,6 +2,9 @@ package com.pumbank.services;
 
 import java.util.List;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
 
@@ -10,19 +13,16 @@ import com.pumbank.models.Padre;
 import com.pumbank.models.StatusMessage;
 import com.pumbank.persistance.HijoManager;
 import com.pumbank.persistance.PadreManager;
-import com.ricardo.persistence.PedidosManager;
-
-
 
 public class AnaService {
-	
+
 //padre/{pid}
-@GET
-	public Response getPadre(@PathParam("pid") int pid) throws Exception {
-		
+	@GET
+	public Response getPadre(@PathParam("pid") int pid) {
+
 		Response resp = null;
 		Padre padre = PadreManager.getInstance().getPadre(pid);
-		
+
 		if (padre == null) {
 			resp = Response.status(404).entity(new StatusMessage(404, "El usuario padre no existe")).build();
 		} else {
@@ -31,12 +31,10 @@ public class AnaService {
 
 		return resp;
 	}
-}
-
 
 //padre/{pid}/hijos
 @GET
-public Response getHijos(@PathParam("pid") int pid) throws Exception) {
+public Response getHijos(@PathParam("pid") int pid) {
 	Padre padre = PadreManager.getInstance().getPadre(pid);
 	
 	List<Hijx> hijos = padre.getHijos();
@@ -44,19 +42,46 @@ public Response getHijos(@PathParam("pid") int pid) throws Exception) {
 	return Response.status(200).entity(hijos).build();
 }
 
-@POST
-public Response addHijo(Hijx hijo) {
-	Response resp = null;
-	
-	if (hijo.validate()) {
-		HijoManager.getInstance().createHijo(hijo);
-		
-		resp = Response.status(200).entity(hijo).build();
-		
-	} else {
-		resp = Response.status(400).entity(new StatusMessage(400, "No se han rellenado los datos correctamente")).build();
+	@POST
+	public Response addHijo(Hijx hijo) {
+		Response resp = null;
+
+		if (hijo.validate()) {
+
+			int hid = HijoManager.getInstance().createHijo(hijo);
+
+			hijo.setHid(hid);
+
+			resp = Response.status(200).entity(hijo).build();
+
+		} else {
+			resp = Response.status(400).entity(new StatusMessage(400, "No se han rellenado los datos correctamente"))
+					.build();
+		}
+
+		return resp;
 	}
 
-	return resp;
-}
+//padre/{pid}/hijos/{hid}
+	public Response getHijo(@PathParam("hid") int hid) {
 
+		Response resp = null;
+		Hijx hijo = HijoManager.getInstance().getHijo(hid);
+
+		if (hijo == null) {
+			resp = Response.status(404).entity(new StatusMessage(404, "El hijo no existe")).build();
+		} else {
+			resp = Response.status(200).entity(hijo).build();
+		}
+
+		return resp;
+
+	}
+
+@PUT
+public boolean actualizarHijo(@PathParam("hid") int hid, Hijx hijo) {
+	
+	HijoManager.getInstance().updateHijo(hijo);
+	
+	return true;
+}
